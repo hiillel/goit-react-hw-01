11e27b8ad2a2c840a2bc837683420f6502009d2c
hiillel <vicky.hillel26@gmail.com>
2024-04-16 23:12:18 +0200

    done

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,28 +1,29 @@
-/* eslint-disable no-unused-vars */
-import React from 'react';
-import css from './TransactionHistory.module.css';
+
+import css from "./TransactionHistory.module.css";
 
 const TransactionHistory = ({ items }) => {
   return (
-    <table className={css.history}>
-      <thead className={css.headerRow}>
-        <tr>
-          <th className={css.type}>Type</th>
-          <th className={css.amount}>Amount</th>
-          <th className={css.currency}>Currency</th>
-        </tr>
-      </thead>
-
-      <tbody className={css.dataRow}>
-        {items.map((transaction) => (
-          <tr key={transaction.id}>
-            <td>{transaction.type}</td>
-            <td>{transaction.amount}</td>
-            <td>{transaction.currency}</td>
+    <div className={css.wrapper}>
+      <table className={css.history}>
+        <thead className={css.headerRow}>
+          <tr>
+            <th className={css.type}>Type</th>
+            <th className={css.amount}>Amount</th>
+            <th className={css.currency}>Currency</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+
+        <tbody className={css.dataRow}>
+          {items.map((transaction) => (
+            <tr key={transaction.id}>
+              <td>{transaction.type}</td>
+              <td>{transaction.amount}</td>
+              <td>{transaction.currency}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
